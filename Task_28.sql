a4c6234bd5813e44471d77d7a63ea2d7e460b45d
Katukan <notfullin.ilnaz@gmail.com>
2021-12-25 13:23:52 +0300

    Task_28 is ready

--- Task_28.sql
+++ Task_28.sql
@@ -4,3 +4,7 @@
 
 ***************************************************************************/
 
+SELECT COUNT(maker) AS qty
+FROM (SELECT maker AS maker, COUNT(model) AS count_model 
+      FROM Product GROUP BY maker) AS [new_product]
+WHERE count_model = 1
